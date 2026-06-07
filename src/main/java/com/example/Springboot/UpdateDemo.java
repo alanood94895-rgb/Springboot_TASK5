9dce46d9067a058d3d89886529ecf40f48c4eaa0
CompanyProfile <alanood94895@gmail.com>
2026-06-07 09:35:41 +0400

    Display original name

--- src/main/java/com/example/Springboot/UpdateDemo.java
+++ src/main/java/com/example/Springboot/UpdateDemo.java
@@ -7,6 +7,7 @@ public class UpdateDemo {
         String updateStatus;
 
         System.out.println("Original Student Name: " + currentStudentName);
+        System.out.println("Original Student Name: " + newStudentName);
 
     }
 
