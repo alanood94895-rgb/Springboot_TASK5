4a5c2685404e6565061d4809bb6ae9009b004252
CompanyProfile <alanood94895@gmail.com>
2026-06-07 10:05:14 +0400

    Create Student object with initial values

--- src/main/java/com/example/Springboot/StudentController.java
+++ src/main/java/com/example/Springboot/StudentController.java
@@ -3,6 +3,8 @@ package com.example.Springboot;
 public class StudentController {
     public static void main(String[] args) {
 
+        Student student = new Student(101, "Ahmed", "A");
+
     }
 }
 
