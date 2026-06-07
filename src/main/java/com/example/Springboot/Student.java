0f142e6e230d90b480fe54f95392aa906a2fd22d
CompanyProfile <alanood94895@gmail.com>
2026-06-07 10:02:18 +0400

    Add Getter and Setter

--- src/main/java/com/example/Springboot/Student.java
+++ src/main/java/com/example/Springboot/Student.java
@@ -6,7 +6,31 @@ public class Student {
     private String StudentName;
     private String Grade;
 
-    public Student (Integer StudentId,String StudentName,String Grade){
+    public Integer getStudentId() {
+        return StudentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        StudentId = studentId;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public String getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(String grade) {
+        Grade = grade;
+    }
+
+    public Student (Integer StudentId, String StudentName, String Grade){
         this.StudentId = StudentId;
         this.StudentName = StudentName;
         this.Grade = Grade;
